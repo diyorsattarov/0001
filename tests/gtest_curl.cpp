b5b36cf814899d3755f6ee4c6b7a8cd9327e9103
diyor <diyorsattarov7@gmail.com>
2023-07-23 12:13:39 -0500

    Not working -- passes first CurlVersion test, fails httpgetrequest test

--- tests/gtest_curl.cpp
+++ tests/gtest_curl.cpp
@@ -1,13 +1,57 @@
 #include "gtest_curl.h"
 
-TEST_F(CurlVersionTest, CheckCurlVersion) 
+std::string performHttpGet(const std::string& url) 
 {
-    // Get the libcurl version
+    std::string response;
+
+    CURL* curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, [](char* data, size_t size, size_t nmemb, std::string* response) {
+            size_t totalSize = size * nmemb;
+            response->append(data, totalSize);
+            return totalSize;
+        });
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+        CURLcode res = curl_easy_perform(curl);
+        if (res != CURLE_OK) {
+            // Handle error if needed
+            // For simplicity, we'll just set an empty response in case of an error
+            response = "";
+        }
+
+        curl_easy_cleanup(curl);
+    }
+
+    return response;
+}
+
+
+TEST_F(CurlTest, CheckCurlVersion) 
+{
     const char* versionStr = curl_version();
     
-    // Print the libcurl version
     std::cout << "libcurl version: " << versionStr << std::endl;
 
-    // Perform the test - You can use EXPECT or ASSERT based on your needs
     EXPECT_TRUE(versionStr != nullptr);
+}
+
+
+// Test case for HTTP GET request
+TEST_F(CurlTest, HttpGetRequest) 
+{
+    // Replace this URL with the URL you want to test
+    std::string url = "https://www.example.com";
+
+    // Perform the HTTP GET request
+    std::string response = performHttpGet(url);
+
+    // Output the response for debugging (optional)
+    std::cout << "HTTP Response: " << response << std::endl;
+
+    // Add your assertions to check the response
+    // For example, you can check if the response contains specific data or if the status code is as expected
+    EXPECT_FALSE(response.empty());
 }
--- tests/gtest_curl.h
+++ tests/gtest_curl.h
@@ -3,18 +3,19 @@
 
 #include <gtest/gtest.h>
 #include <curl/curl.h>
+#include <string>
 
 // Test fixture for libcurl version check
-class CurlVersionTest : public ::testing::Test 
+class CurlTest : public ::testing::Test 
 {
 protected:
-    static void SetUpTestCase() {
-        // Initialize libcurl
+    static void SetUpTestCase() 
+    {
         curl_global_init(CURL_GLOBAL_ALL);
     }
 
-    static void TearDownTestCase() {
-        // Cleanup libcurl
+    static void TearDownTestCase() 
+    {
         curl_global_cleanup();
     }
 };
