a4943fe3145086d1b2e580b7ce3c7931665117d8
diyor <diyorsattarov7@gmail.com>
2023-07-23 20:40:15 -0500

    using boost/chrono -- BoostElapsedTime

--- tests/gtest_boost.cpp
+++ tests/gtest_boost.cpp
@@ -1,7 +1,7 @@
 #include "gtest_boost.h"
 
 // Test case for checking the Boost version
-TEST_F(BoostTest, CheckBoostVersion) 
+TEST_F(BoostTest, BoostVersion) 
 {
     std::string boostVersionStr = "Boost version: " + std::to_string(BOOST_VERSION / 100000) + "." +
                                    std::to_string(BOOST_VERSION / 100 % 1000) + "." +
@@ -11,4 +11,21 @@ TEST_F(BoostTest, CheckBoostVersion)
 
     // Add your assertions to check the Boost version, e.g., comparing with a required version
     EXPECT_TRUE(BOOST_VERSION >= 107500); // For example, check if the Boost version is at least 1.75.0
+}
+
+TEST_F(BoostTest, BoostElapsedTime)
+{
+    std::cout << "Press Enter to stop the timer..." << std::endl;
+
+    boost::chrono::high_resolution_clock::time_point startTime = boost::chrono::high_resolution_clock::now();
+
+    // Wait for user input (Press Enter)
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    boost::chrono::high_resolution_clock::time_point endTime = boost::chrono::high_resolution_clock::now();
+
+    // Calculate the elapsed time in microseconds
+    boost::chrono::microseconds elapsedMicroseconds = boost::chrono::duration_cast<boost::chrono::microseconds>(endTime - startTime);
+    EXPECT_GT(elapsedMicroseconds.count(), 0);
+    std::cout << "Elapsed time: " << elapsedMicroseconds.count() / 1000000.0 << std::endl;
 }
--- tests/gtest_boost.h
+++ tests/gtest_boost.h
@@ -3,7 +3,9 @@
 
 #include <gtest/gtest.h>
 #include <boost/version.hpp>
+#include <boost/chrono.hpp>
 #include <string>
+#include <conio.h>
 
 // Test fixture for libcurl version check
 class BoostTest : public ::testing::Test 
